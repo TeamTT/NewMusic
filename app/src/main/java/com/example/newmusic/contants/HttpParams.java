4751c4e9589be45604ebf9f4a10ed9d1a63f72a4
a376051132 <376051132@qq.com>
2016-11-28 09:32:31 +0800

    create utils

---
+++ app/src/main/java/com/example/newmusic/contants/HttpParams.java
@@ -0,0 +1,10 @@
+package com.example.newmusic.contants;
+
+
+public class HttpParams {
+
+    public static final String CACHE_CONTROL = "Cache-Control";
+
+    public static final String PARMA = "Parma";
+
+}
